e8a18e5895ca8d96ca50897577bfa299dad773cd
oknyawira@gmail.com <oknyawira@gmail.com>
2022-08-26 09:58:27 +0300

    Change player after every move

--- main.py
+++ main.py
@@ -2,37 +2,45 @@
 #block in board : corresponding move made
 
 Board = {
-    '7': '', '8': '', '9': '',
-    '4': '', '5': '', '6': '',
-    '1': '', '2': '', '3': ''
+    '7': ' ', '8': ' ', '9': ' ',
+    '4': ' ', '5': ' ', '6': ' ',
+    '1': ' ', '2': ' ', '3': ' '
 }
-#3x3
+
+board_keys = []
+
+for key in Board:
+    board_keys.append(key)
+
+#print board after every move
 def printBoard(board):
-    print(board['7'] + ' |' + board['8'] + ' |' + board['9'])
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
     print('-+-+-')
-    print(board['4'] + ' |' + board['5'] + ' |' + board['6'])
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
     print('-+-+-')
-    print(board['1'] + ' |' + board['2'] + ' |' + board['3'])
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
 
 
 #take input from user and verify
 def game():
     turn = 'X'
     count = 0
-
-    for (i) in range(10):
+    
+    i = 1
+    while (i<10):
+    #while True:
+    #for (i) in range(10):
         printBoard(Board)
-        print("Make your move" + turn)
+        print("Your turn " + turn + " Make a move")
 
         move = input()
 
         if Board[move] == ' ':
             Board[move] = turn
             count += 1
-
         else:
             print("That position is already filled.\nMove to another position")
-            continue
+
 
 #Check if either player has won after 5 moves
         if count >= 5:
@@ -52,17 +60,17 @@ def game():
                 print(" **** " +turn + " won. ****")
                 break
 
-            elif Board['7'] == Board['4'] == Board['1'] != ' ': # vertical_left-side
+            elif Board['1'] == Board['4'] == Board['7'] != ' ': # vertical_left-side
                 printBoard(Board)
                 print("\nGame Over.\n")                
                 print(" **** " +turn + " won. ****")
                 break
-            elif Board['8'] == Board['5'] == Board['2'] != ' ': # vertical_middle
+            elif Board['2'] == Board['5'] == Board['8'] != ' ': # vertical_middle
                 printBoard(Board)
                 print("\nGame Over.\n")                
                 print(" **** " +turn + " won. ****")
                 break
-            elif Board['9'] == Board['6'] == Board['3'] != ' ': # vertical right-side
+            elif Board['3'] == Board['6'] == Board['9'] != ' ': # vertical right-side
                 printBoard(Board)
                 print("\nGame Over.\n")                
                 print(" **** " +turn + " won. ****")
@@ -78,15 +86,22 @@ def game():
                 print("\nGame Over.\n")                
                 print(" **** " +turn + " won. ****")
                 break 
+            
         
 
-        # If neither wins and the board is full, declare as 'tie'.
+        # If neither wins and the board is full, declare 'tie'.
         if count == 9:
             print("\nGame Over.\n")                
             print("It's a Tie!!")
+            break
 
         # change the player after every move.
         if turn =='X':
             turn = 'O'
         else:
             turn = 'X' 
+
+
+        
+game()
+  
